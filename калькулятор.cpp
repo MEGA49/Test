873ed466b27f4b2e17d7ccbbf8973e49037d492c
MEGA49 <168743043+MEGA49@users.noreply.github.com>
2024-12-13 18:36:27 +0300

    Add files via upload

---
+++ калькулятор.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	system("chcp 1251");
+	char a;
+	int b, c;
+	cout << "Введи знак действия \n";
+	cin >> a;
+	cout << "введи первое число \n";
+	cin >> b;
+	cout << "введите второе число \n";
+	cin >> c;
+	switch (a) {
+	case '+': cout << "результат " << b + c << '\n';
+		break;
+	case '-': cout << "результат " << b - c << '\n';
+		break;
+	case '*': cout << "результат " << b * c << '\n';
+		break;
+	case '/': 
+	
+		if (a == '/' && c == 0) {
+			cout << "так нельзя";
+		}
+		cout << "результат " << b / c << '\n';
+		break;
+	}
+	system("pause");
+	return 0;
+
+}
